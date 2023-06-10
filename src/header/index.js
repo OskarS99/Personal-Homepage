4b0069fd3dbdaa190b097bc168624f592838e352
Oskar Siry <oskarsiry99@gmail.com>
2023-06-10 18:22:18 +0200

    new elemnt added to component

--- src/header/index.js
+++ src/header/index.js
@@ -1,15 +1,13 @@
 export const Header = () => {
-  return (
+  <div>
+    <img src="src/oskar.jpg" alt="Oskar Siry" />
     <div>
-      <img src="src/oskar.jpg" alt="Oskar Siry" />
       <caption>This is</caption>
       <h1>Oskar Siry</h1>
       <p>
-        {" "}
-        👨🏻‍💻 I’m a passionate Frontend Developer in love with React, currently
+        👨🏻Im a passionate Frontend Developer in love with React, currently
         looking for new job opportunities.
       </p>
-      <button></button>
     </div>
-  );
+  </div>
 };

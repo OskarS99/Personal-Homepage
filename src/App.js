2aa507e2f41aefddcd5ec8adeb025ff1acb45a33
Oskar Siry <oskarsiry99@gmail.com>
2023-06-20 17:48:27 +0200

    new components added to app

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from "react";
 import { Header } from "./header";
 import { GlobalStyle } from "./globalStyle";
 import Container from "./Container";
+import { Skillset } from "./Skillset";
+import { Learn } from "./Learn";
+import { GitProjects } from "./GitProjects";
+import { Contact } from "./Contact";
 
 const App = () => (
   <Container>
     <GlobalStyle />
     <Header />
+    <Skillset />
+    <Learn />
+    <GitProjects />
+    <Contact />
   </Container>
 );
 export default App;

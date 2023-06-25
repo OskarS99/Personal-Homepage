f4faf5d5b5367c4f81df6a3d3b1993e2054ae6bd
Oskar Siry <oskarsiry99@gmail.com>
2023-06-25 20:25:05 +0200

    theme for light and dark mode created

---
+++ src/theme.js
@@ -0,0 +1,105 @@
+export const lightTheme = {
+  color: {
+    body: `#FBFBFE`,
+    header: `#252525`,
+    text: `#6E7E91`,
+    iconGit: `#0366D6`,
+    projectHeader: `#0366D6`,
+  },
+
+  box: {
+    color: `#FFFFFF`,
+    shadow: `0px 16px 58px 0px rgba(9, 10, 51, 0.03),
+       0px -2px 50px 0px rgba(9, 10, 51, 0.02)`,
+    hover: `6px solid rgba(3, 102, 214, 0.2)`,
+  },
+
+  mail: {
+    color: `#252525`,
+    hover: `#2188FF`,
+  },
+
+  circle: {
+    elipse1: `#D1D5DA`,
+    elipse2: `#0366D6`,
+  },
+
+  button: {
+    color: `#0366D6`,
+    border: `1px solid rgba(209, 213, 218, 0.3)`,
+
+    focus: `-2px -2px 0px 0px rgba(140, 194, 255, 1),
+
+    2px 2px 0px 0px rgba(140, 194, 255, 1),
+    
+     -2px 2px 0px 0px rgba(140, 194, 255, 1),
+    
+     2px -2px 0px 0px rgba(140, 194, 255, 1)
+    
+    `,
+
+    selected: `0px 2px 0px 0px rgba(20, 70, 32, 0.2) inset
+    `,
+  },
+
+  link: {
+    color: `#0366D6`,
+  },
+
+  socialMedia: {
+    color: `#252525`,
+    focus: `#0366D6`,
+  },
+};
+export const darkTheme = {
+  color: {
+    body: `#252525`,
+    header: `#FFFFFF`,
+    text: `#FFFFFF`,
+    iconGit: `#2188FF`,
+    projectHeader: `#FFFFFF`,
+  },
+
+  box: {
+    color: `rgba(54, 54, 54, 0.72)`,
+
+    shadow: ` 0px 16px 58px 0px rgba(9, 10, 51, 0.03),
+      0px -2px 50px 0px rgba(9, 10, 51, 0.02)
+    `,
+    hover: `6px solid rgba(3, 102, 214, 0.5)`,
+  },
+
+  mail: {
+    color: `#FFFFFF`,
+    hover: `#0366D6`,
+  },
+  circle: {
+    elipse1: `#363636`,
+    elipse2: `#2188FF`,
+  },
+
+  button: {
+    color: `#2188FF`,
+    border: `1px solid rgba(209, 213, 218, 0.1)`,
+    focus: ` -2px -2px 0px 0px rgba(109, 147, 190, 1),
+
+    2px 2px 0px 0px rgba(109, 147, 190, 1),
+    
+ -2px 2px 0px 0px rgba(109, 147, 190, 1),
+    
+   2px -2px 0px 0px rgba(109, 147, 190, 1)
+    `,
+
+    selected: `0px 2px 0px 0px rgba(20, 70, 32, 0.2) inset
+    `,
+  },
+
+  link: {
+    color: `#2188FF`,
+  },
+
+  socialMedia: {
+    color: `#FFFFFF`,
+    focus: `#2188FF`,
+  },
+};

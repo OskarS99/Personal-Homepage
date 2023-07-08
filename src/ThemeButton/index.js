d5bc65fcb5120f95be049d92965243dfebaf139e
Oskar Siry <oskarsiry99@gmail.com>
2023-07-08 15:09:28 +0200

    name changed from "theme" to "darkTheme"

--- src/ThemeButton/index.js
+++ src/ThemeButton/index.js
@@ -1,21 +1,20 @@
 import { useDispatch, useSelector } from "react-redux";
-import { Button, Caption, Wrapper,} from "./styled";
-import { selectTheme, themeToggler } from "../themeSlice";
+import { Button, Caption, Wrapper } from "./styled";
+import { selectDarkTheme, toggleDarkTheme } from "../themeSlice";
 import { ReactComponent as LightThemeButton } from "../SVG/LightButton.svg";
 import { ReactComponent as DarkThemeButton } from "../SVG/DarkButton.svg";
 
 export const ThemeButton = () => {
-  const theme = useSelector(selectTheme);
+  const darkTheme = useSelector(selectDarkTheme);
   const dispatch = useDispatch();
-
-  return (    
-  <Wrapper>
-       <Caption>
-        Dark mode <span>{theme.theme === true ? "off" : "on"}</span>
+  return (
+    <Wrapper>
+      <Caption>
+        Dark mode <span>{darkTheme.darkTheme ? "on" : "off"}</span>
       </Caption>
-   <Button onClick={() => dispatch(themeToggler())}>
-        {theme.theme === true ? <LightThemeButton/> : <DarkThemeButton/>}
-     </Button>
-   </Wrapper>
-  );  
+      <Button onClick={() => dispatch(toggleDarkTheme())}>
+        {darkTheme.darkTheme ? <DarkThemeButton /> : <LightThemeButton />}
+      </Button>
+    </Wrapper>
+  );
 };

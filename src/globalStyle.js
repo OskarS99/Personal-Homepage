d0dface9f547c5d422372ec4426c51727e8ce7ea
Oskar Siry <oskarsiry99@gmail.com>
2023-07-11 13:42:29 +0200

    code formatted

--- src/globalStyle.js
+++ src/globalStyle.js
@@ -2,17 +2,16 @@ import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
 html {
-box-sizing: border-box;
-
+  box-sizing: border-box;
 }
 
 *, ::after, ::before {
-box-sizing: inherit;
+  box-sizing: inherit;
 }
 
 body {
-font-family: 'Inter', sans-serif;
-transition: background-color 0.3s;
-background-color: ${(props) => props.theme.color.body};
+  font-family: 'Inter', sans-serif;
+  transition: background-color 0.3s;
+  background-color: ${(props) => props.theme.color.body};
 }
 `;

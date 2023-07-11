b4ad8a6a4ab86cf13820761674e3d38693ccaa50
Oskar Siry <oskarsiry99@gmail.com>
2023-07-11 13:31:44 +0200

    unnecessary element deleted

--- src/GitProjects/Project/index.js
+++ src/GitProjects/Project/index.js
@@ -6,22 +6,20 @@ export const Project = () => {
 
   return reposData.map((reposElement) => (
     <Wrapper key={reposElement.id}>
-      <div>
-        <Title>{reposElement.name}</Title>
-        <Description>{reposElement.description}</Description>
-        <LinkWrapper>
-          <Text>
-            Demo:
-            <Link
-              href={`https://oskars99.github.io/${reposElement.name}/#/`}
-            >{`https://oskars99.github.io/${reposElement.name}/#/`}</Link>
-          </Text>
-          <Text>
-            Code:
-            <Link href={reposElement.html_url}>{reposElement.html_url}</Link>
-          </Text>
-        </LinkWrapper>
-      </div>
+      <Title>{reposElement.name}</Title>
+      <Description>{reposElement.description}</Description>
+      <LinkWrapper>
+        <Text>
+          Demo:
+          <Link
+            href={`https://oskars99.github.io/${reposElement.name}/#/`}
+          >{`https://oskars99.github.io/${reposElement.name}/#/`}</Link>
+        </Text>
+        <Text>
+          Code:
+          <Link href={reposElement.html_url}>{reposElement.html_url}</Link>
+        </Text>
+      </LinkWrapper>
     </Wrapper>
   ));
 };
